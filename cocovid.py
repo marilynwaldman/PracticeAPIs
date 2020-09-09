82d69206a632641dfaa31d741cf3362553c57f0b
MarilynWaldman <mdwaldman22@hotmail.com>
2020-09-09 08:27:45 -0600

    added api to get data from the state

--- cocovid.py
+++ cocovid.py
@@ -1,1 +1,12 @@
-#Get Colorado Covid Data
+#Get Colorado Covid Data from the State of Colorado
+import requests
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
+url = "http://data.sfgov.org/resource/rqzj-sfat.json"
+r = requests.get(url)
+print(type(r))
+data = r.json()
+pp.pprint(data)
+
